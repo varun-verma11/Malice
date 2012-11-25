b9a60e05daaa717119128e8e946df0855f855cea
Magda Gocek <mg2111@ic.ac.uk>
2012-11-25 05:51:56 +0000

    Worked on the nested scopes in SymbolTable

--- Compiler/MAlice/src/symbol_table/StatementChecker.java
+++ Compiler/MAlice/src/symbol_table/StatementChecker.java
@@ -4,16 +4,33 @@ import org.antlr.runtime.tree.Tree;
 
 public class StatementChecker {
 	
-	public static void checkStatement(Tree node)
+	public static void checkStatement(Tree node, SymbolTable symbolTable)
 	{
-		if ( node.getText().contentEquals("was"))
+		if (node.getText().contentEquals("was"))
 		{
 			if (!checkVarInScope(node.getChild(0).getText(), symbolTable))
 			{
 				//add this var to ur table
 			}
 			
+		}
+		
+		if (node.getText().contentEquals("became"))
+		{
+			checkIfDataTypeIsInTheRange(node);
+			checkIfTypesMatch(node, symbolTable);
 			
 		}
+	}
+
+	private static void checkIfTypesMatch(Tree node, SymbolTable symbolTable) {
+		
+		((VariableSTValue)symbolTable.lookup(node.getChild(0).getText())).getType();//type x == number
+		node.getChild(1)//symTable lookup this node?
+		
+	}
+
+	private static void checkIfDataTypeIsInTheRange(Tree node) {
+		//check and print errors
 	}
 }
--- Compiler/MAlice/src/symbol_table/SymbolTable.java
+++ Compiler/MAlice/src/symbol_table/SymbolTable.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 public class SymbolTable implements SymbolTableInterface<String, SymbolTableValue>
 {
-	Map<String,SymbolTableValue> symbolTable = new HashMap<String, SymbolTableValue>();
+	private Map<String,SymbolTableValue> symbolTable = new HashMap<String, SymbolTableValue>();
+	private SymbolTable enclosingSymbolTable;// = new SymbolTable();
+	private SymbolTable currentLevelTable; //is this just the symbolTable field above?? i.e. = symbolTable
+	
 	
 	public boolean checkVariableScopeInFunctionInCurrLevel(String var, String f_name)
 	{
@@ -28,14 +31,28 @@ public class SymbolTable implements SymbolTableInterface<String, SymbolTableValu
 	}
 	
 	@Override
-	public void insert(String name, SymbolTableValue value) {
+	public void insert(String name, SymbolTableValue value) 
+	{
 		symbolTable.put(name, value);		
 	}
 
-	@Override
-	public SymbolTableValue lookup(String name) {
+	@Override  //need to change this so that it makes sure it looks in other scopes!!!!
+	public SymbolTableValue lookup(String name) 
+	{
 		return symbolTable.get(name);
 	}
 
+	public void initializeScope() 
+	{
+		SymbolTable newTable = new SymbolTable();
+		newTable.enclosingSymbolTable = currentLevelTable;
+		this.currentLevelTable = newTable;
+		
+	}
+	
+	public void finalizeScope()
+	{
+		this.currentLevelTable = enclosingSymbolTable;
+	}
 
 }
