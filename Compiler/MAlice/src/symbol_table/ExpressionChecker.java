d50a9f93435cdeb4ca84513781e4ed8996a6a9d0
Varun Verma <vv311@ic.ac.uk>
2012-11-25 01:02:57 +0000

    started the class for checking of the expressions

---
+++ Compiler/MAlice/src/symbol_table/ExpressionChecker.java
@@ -0,0 +1,16 @@
+package symbol_table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.antlr.runtime.tree.CommonTree;
+
+public class ExpressionChecker 
+{
+	private Map<String,DATA_TYPES> operators_map = new HashMap<String, DATA_TYPES>();
+	
+	public ExpressionChecker(CommonTree t) 
+	{
+		
+	}
+}
