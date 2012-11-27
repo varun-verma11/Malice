8018745ba94d463d7f99c0cfa4b289825a5928e3
Varun Verma <vv311@ic.ac.uk>
2012-11-27 22:43:34 +0000

    forgot to add the file which had the newest version for checking of the statements

--- Compiler/MAlice/src/semantics_checks/SemanticsUtils.java
+++ Compiler/MAlice/src/semantics_checks/SemanticsUtils.java
@@ -7,14 +7,7 @@ import symbol_table.DATA_TYPES;
 public class SemanticsUtils {
 	public static DATA_TYPES getReturnType(Tree node)
 	{
-		Tree curr = node.getChild(0);
-
-		while (curr != null && curr.getText().contentEquals("contained"))
-		{
-			curr = getNextChild(curr);
-		}
-
-		return DATA_TYPES.valueOf(curr.getText().toUpperCase());
+		return DATA_TYPES.valueOf(node.getText().toUpperCase());
 	}
 
 	public static Tree getNextChild(Tree current)
