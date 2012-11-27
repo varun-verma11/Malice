24ad4a48e13d6b0bc7b2dd641bf320c9f8963893
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-11-27 18:44:20 +0000

    Statement Error Checking done, hopefully

--- Compiler/MAlice/src/symbol_table/StatementChecker.java
+++ Compiler/MAlice/src/symbol_table/StatementChecker.java
@@ -3,7 +3,10 @@ package symbol_table;
 
 import org.antlr.runtime.tree.Tree;
 
-public class StatementChecker {
+import semantics_checks.SemanticsUtils;
+
+public class StatementChecker
+{
 
 	public static Tree checkAllStatements(Tree node, SymbolTable table) 
 	{
@@ -20,6 +23,7 @@ public class StatementChecker {
 	}
 
 	private static Tree getNextChild(Tree current)
+
 	{
 		return current.getParent().getChild(current.getChildIndex()+1);
 	}
@@ -28,23 +32,33 @@ public class StatementChecker {
 	public static boolean checkStatement(Tree node, SymbolTable symbolTable)
 	{
 		if (node.getChildCount()==0) { return false; }
-		String var = node.getChild(0).getText();
 		int currentScopeLevel = symbolTable.getCurrentScopeLevel();
-		
+
 
 		if (node.getText().contentEquals("was"))
 		{
-
+			String var = node.getChild(0).getText();
+			
 			if (symbolTable.checkVariableIsInCurrentScopeLevel(var))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
 						+ node.getCharPositionInLine() 
-						+ " Multiple declarations of " + node.getText());
+						+ " Multiple declarations of " + var);
+			}
+			
+			else if (node.getChildCount() > 2 &&
+						( ExpressionChecker.getExpressionType(node.getChild(2), symbolTable) !=
+						(SemanticsUtils.getReturnType(node.getChild(1))))
+					)
+			{
+				System.err.println("Line "+ node.getLine()+ ": " 
+						+ node.getCharPositionInLine() 
+						+ "Data types of " + var + " and subscribed value don't match.");
 			}
 
 			else 
 			{
-				DATA_TYPES type = DATA_TYPES.valueOf((node.getChild(1).getText()).toUpperCase());
+				DATA_TYPES type = ExpressionChecker.getExpressionType(node.getChild(1), symbolTable);
 				symbolTable.insert(var, new VariableSTValue(type, false));
 			}
 
@@ -54,7 +68,7 @@ public class StatementChecker {
 
 		else if ( node.getText().contentEquals("ate") ||  node.getText().contentEquals("drank"))
 		{	
-
+			String var = node.getChild(0).getText();
 			if (!symbolTable.checkVariableIsInOtherScopeLevels(var, currentScopeLevel))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
@@ -82,7 +96,7 @@ public class StatementChecker {
 
 		else if ( node.getText().contentEquals("became"))
 		{
-
+			String var = node.getChild(0).getText();
 			if (!symbolTable.checkVariableIsInOtherScopeLevels(var, currentScopeLevel))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
@@ -90,25 +104,35 @@ public class StatementChecker {
 						+ var +" out of scope" + node.getText());
 			}
 
-			else if ((symbolTable.lookup(node.getChild(0).getText())).getType() != ExpressionChecker.getExpressionType(node.getChild(1), symbolTable))
+			else if ((symbolTable.lookup(node.getChild(0).getText())).getType() !=
+					ExpressionChecker.getExpressionType(node.getChild(node.getChildCount() - 1), symbolTable))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
 						+ node.getCharPositionInLine() + " : Types of "
 						+ var + " and the subcribed value don't match.");
 			}
 
+			else if (node.getChildCount() > 2 &&
+					ExpressionChecker.getExpressionType(node.getChild(1), symbolTable) != DATA_TYPES.NUMBER)
+			{
+				System.err.println("Line "+ node.getLine()+ ": " 
+						+ node.getCharPositionInLine() + " : Length of the Array" + var + " should be an Integer");
+			}
+
 			else
 			{
+
 				((VariableSTValue) symbolTable.lookup(node.getChild(0).getText())).setInitialised(true) ;
+
 			}
 
 			return true;
-
 		}
 
 		else if ( node.getText().contentEquals("spoke") || node.getText().contentEquals("said"))
-		{
 
+		{
+			String var = node.getChild(0).getText();
 			if (!symbolTable.checkVariableIsInOtherScopeLevels(var, currentScopeLevel))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
@@ -121,6 +145,7 @@ public class StatementChecker {
 				System.err.println("Line "+ node.getLine()+ ": " 
 						+ node.getCharPositionInLine() + ": "
 						+ var +" not initialised yet" + node.getText());				
+
 			}
 
 			return true;
@@ -129,13 +154,14 @@ public class StatementChecker {
 		else if ( node.getText().contentEquals("what"))
 
 		{
+			String var = node.getChild(0).getText();
 			if (!symbolTable.checkVariableIsInOtherScopeLevels(var, currentScopeLevel))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
 						+ node.getCharPositionInLine() + ": "
 						+ var +" out of scope" + node.getText());				
 			}
-			
+
 			else
 			{
 				((VariableSTValue) symbolTable.lookup(node.getChild(0).getText())).setInitialised(true);
@@ -143,10 +169,11 @@ public class StatementChecker {
 
 			return true;
 		}
-		
+
 		else if ( node.getText().contentEquals("found"))
 
 		{
+			String var = node.getChild(0).getText();
 			if (!symbolTable.checkVariableIsInOtherScopeLevels(var, currentScopeLevel))
 			{
 				System.err.println("Line "+ node.getLine()+ ": " 
@@ -160,16 +187,30 @@ public class StatementChecker {
 						+ node.getCharPositionInLine() + ": "
 						+ var +" not initialised yet" + node.getText());				
 			}
-			
 			return true;
 		}
-		
+
 		else if ( node.getText().contentEquals("had"))
 		{
+			String var = node.getChild(0).getText();
+			
+			if (symbolTable.checkVariableIsInCurrentScopeLevel(var))
+			{
+				System.err.println("Line "+ node.getLine()+ ": " 
+						+ node.getCharPositionInLine() 
+						+ " Multiple declarations of " + node.getText());
+			}
+			
+			if ((symbolTable.lookup(node.getChild(1).getText())).getType() != DATA_TYPES.NUMBER)
+			{
+				System.err.println("Line "+ node.getLine()+ ": " 
+						+ node.getCharPositionInLine() + " : "
+						+ var + " not a number.");
+			}
 			
 		}
 
-		return false;
-	}
+	return false;
+}
 
 }
