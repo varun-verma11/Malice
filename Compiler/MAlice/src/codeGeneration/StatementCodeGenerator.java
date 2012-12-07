7716379757f312cbdd2c9cae825e08c011b9f2bc
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-12-07 19:51:52 +0000

    chnages in StatmentCodeGenerator

--- Compiler/MAlice/src/codeGeneration/StatementCodeGenerator.java
+++ Compiler/MAlice/src/codeGeneration/StatementCodeGenerator.java
@@ -1,71 +1,60 @@
-//package codeGeneration;
-//
-//import java.util.ArrayDeque;
-//import java.util.Queue;
-//
-//import org.antlr.runtime.tree.Tree;
-//
-//import symbol_table.SymbolTable;
-//
-//public class StatementCodeGenerator {
-//	private static int regNumber = 0;
-//	private static Queue<String> statements = new ArrayDeque<String>();
-//	
-//	public static void emptyQueue()
-//	{
-//		statements = new ArrayDeque<String>();
-//		regNumber = 0;
-//	}
-//	
-//	public static String getResultReg(Tree node, SymbolTable table){
-//		if (node==null) return null;
-//		
-//		STATEMENT stat = getStatement(node.getText());
-//		
-//		String arg1 = getResultReg(node.getChild(0), table);
-//		String arg2 = getResultReg(node.getChild(1), table);
-//		String arg3 = getResultReg(node.getChild(2), table);
-//		
-//		String uniqueRegisterID = getUniqueRegisterID();
-//		
-//		switch(stat)
-//		{
-//			case WAS: 
-//				writeWAS(uniqueRegisterID, "was", arg1, arg2);
-//				break;
-//			
-//			case HAD: 
-//				break;
-//			
-//			case WHAT: 
-//				 break;
-//		}
-//		
-//	}
-//	
-//	private static void writeWAS(uniqueRegisterID, , arg1, arg2) {
-//		
-//	}
-//	
-//	public static String getUniqueRegisterID()
-//	{
-//		++regNumber;
-//		return "%"+regNumber;
-//	}
-//	
-//	public static String getLocalRegisterID(int reg)
-//	{
-//		return "%" + reg;
-//	}
-//	
-//	private static STATEMENT getStatement(String s){
-//		if (s.contentEquals("was")) return STATEMENT.WAS;
-//		if (s.contentEquals("had")) return STATEMENT.HAD;
-//		if (s.contentEquals("what")) return STATEMENT.WHAT;
-//		return null;		
-//	}
-//
-//	private enum STATEMENT{
-//		WAS, HAD, WHAT
-//	}
-//}
+package codeGeneration;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import org.antlr.runtime.tree.Tree;
+
+import symbol_table.SymbolTable;
+
+public class StatementCodeGenerator {
+	
+	public static void getStatement(Tree node, SymbolTable table){
+		
+		STATEMENT stat = getStatement(node.getText());
+		
+		String arg1 = node.getChild(0).getText();
+		String arg2 = node.getChild(1).getText();
+		String arg3 = node.getChild(2).getText();
+		
+		String uniqueRegisterID = CodeGenerator.getUniqueRegisterID();
+		
+		switch(stat)
+		{
+			case WAS: 
+				writeWAS(uniqueRegisterID, "was", arg1, arg2);
+				break;
+			
+			case HAD: 
+				break;
+			
+			case WHAT: 
+				 break;
+		}
+		
+	}
+	
+	private static void writeWAS(String uniqueRegisterID, String arg1, String arg2, String arg3) {
+		if (arg2.equals("number")) {
+			CodeGenerator.addInstruction("%" + arg1 + " = alloca i32, align 4");
+		}
+		if (arg2.equals("letter")) {
+			CodeGenerator.addInstruction("%" + arg1 + " = alloca i8, align 1");
+		}
+		if (arg2.equals("sentence")) {
+			CodeGenerator.addInstruction("%" + arg1 + " = alloca i8*, align 8");
+		}
+	}
+	
+
+	private static STATEMENT getStatement(String s){
+		if (s.contentEquals("was")) return STATEMENT.WAS;
+		if (s.contentEquals("had")) return STATEMENT.HAD;
+		if (s.contentEquals("what")) return STATEMENT.WHAT;
+		return null;		
+	}
+
+	private enum STATEMENT{
+		WAS, HAD, WHAT
+	}
+}
