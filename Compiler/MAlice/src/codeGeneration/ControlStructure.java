6e0efcac9be0702b8b51a1972d59d96c3eaf423b
Varun Verma <vv311@ic.ac.uk>
2012-12-08 14:20:39 +0000

    did code genration for the eventually case

--- Compiler/MAlice/src/codeGeneration/ControlStructure.java
+++ Compiler/MAlice/src/codeGeneration/ControlStructure.java
@@ -38,7 +38,7 @@ public class ControlStructure
 		String lblThen = CodeGenerator.getUniqueLabel();
 		int brInsIndex = CodeGenerator.getNumberOfInstructions();
 		int[] endIfLblInserts = new int[2];
-		CodeGenerator.addInstruction("; <label>:" + lblThen.substring(1));
+		CodeGenerator.addInstruction(getLabel(lblThen));
 		// DO ALL STATEMENTS
 		@SuppressWarnings("unused")
 		String temp = Expression.getResultReg(node.getChild(1).getChild(1),
@@ -46,31 +46,56 @@ public class ControlStructure
 		// DO ALL STATEMENTS
 		endIfLblInserts[0] = CodeGenerator.getNumberOfInstructions();
 		String lblElse = CodeGenerator.getUniqueLabel();
-		CodeGenerator.addInstruction("; <label>:" + lblElse.substring(1));
+		CodeGenerator.addInstruction(getLabel(lblElse));
 		// DO ALL STATEMENTS
 		temp = Expression.getResultReg(node.getChild(3).getChild(1), table);
 		// DO ALL STATEMENTS
 		endIfLblInserts[1] = CodeGenerator.getNumberOfInstructions();
 		String lblEndIf = CodeGenerator.getUniqueLabel();
-		CodeGenerator.addInstruction("; <label>:" + lblEndIf);
+		CodeGenerator.addInstruction(getLabel(lblEndIf));
 		CodeGenerator
-				.addInstruction("br label " + lblEndIf, endIfLblInserts[1]);
+				.addInstruction(getBranchIns(lblEndIf), endIfLblInserts[1]);
 		CodeGenerator
-				.addInstruction("br label " + lblEndIf, endIfLblInserts[0]);
-		CodeGenerator.addInstruction("br il " + boolExp + ", label " + lblThen
-				+ ", label " + lblElse, brInsIndex);
+				.addInstruction(getBranchIns(lblEndIf), endIfLblInserts[0]);
+		CodeGenerator.addInstruction(getConditionalBranchIns(boolExp, lblThen, lblElse), brInsIndex);
 	}
 
 	public static void writeEventuallyStatement(Tree node, SymbolTable table)
 	{
-		String boolExp = Expression.getResultReg(node.getChild(0), table);
-		try{
-			int bool = Integer.parseInt(boolExp);
-			if (bool==0) return;
-		} catch (NumberFormatException e) { }
 		String startLbl = CodeGenerator.getUniqueLabel();
-		CodeGenerator.addInstruction("br label " + startLbl);
-		CodeGenerator.addInstruction(";<label>:" + startLbl.substring(1));
-		
+		CodeGenerator.addInstruction(getBranchIns(startLbl));
+		CodeGenerator.addInstruction(getLabel(startLbl));
+		String boolExp = Expression.getResultReg(node.getChild(0), table);
+//		try{
+//			int bool = Integer.parseInt(boolExp);
+//			if (bool==0) return;
+//		} catch (NumberFormatException e) { }
+		int brInsIndex = CodeGenerator.getNumberOfInstructions();
+		String loop = CodeGenerator.getUniqueLabel();
+		CodeGenerator.addInstruction(getLabel(loop));
+		//DO ALL STATEMENTSS
+		@SuppressWarnings("unused")
+		String temp = Expression.getResultReg(node.getChild(1).getChild(1), table);
+		//DO ALL STATEMENTSS
+		CodeGenerator.addInstruction(getBranchIns(startLbl));
+		String endLoop = CodeGenerator.getUniqueLabel();
+		CodeGenerator.addInstruction(getLabel(endLoop));
+		CodeGenerator.addInstruction(getConditionalBranchIns(boolExp, loop,endLoop), brInsIndex);
+	}
+
+	private static String getConditionalBranchIns(String boolExp, String reg1,
+			String reg2)
+	{
+		return "br il " + boolExp + ", label " + reg1 + ", label " + reg2;
+	}
+	
+	private static String getBranchIns(String lbl)
+	{
+		return "br label " + lbl;
+	}
+	
+	private static String getLabel(String lbl)
+	{
+		return ";<label>:" + lbl.substring(1);
 	}
 }
