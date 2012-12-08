b5b5d462543a7d25f93d30d083432957ee0f6609
x <mgaegapcdi@ubuntu.ubuntu-domain>
2012-12-08 14:25:07 +0000

    Worked on a class that performs code generation for various statements, including became, ate, drank, found and spoke/said Alice. Harsh is working on the other statements and once we have finished we will combine our classes together.

--- Compiler/MAlice/src/semantics_checks/StatementChecker.java
+++ Compiler/MAlice/src/semantics_checks/StatementChecker.java
@@ -2,6 +2,8 @@ package semantics_checks;
 
 import org.antlr.runtime.tree.Tree;
 
+import codeGeneration.StatementsCodeGeneratorMagda;
+
 import symbol_table.CodeBlockSTValue;
 import symbol_table.DATA_TYPES;
 import symbol_table.FunctionSTValue;
@@ -9,18 +11,14 @@ import symbol_table.SymbolTable;
 import symbol_table.SymbolTableValue;
 import symbol_table.VariableSTValue;
 
-public class StatementChecker
-{
+public class StatementChecker {
 
-	public static Tree checkAllStatements(Tree node, SymbolTable table)
-	{
+	public static Tree checkAllStatements(Tree node, SymbolTable table) {
 		Tree current = node;
 		boolean end_of_statements = false;
-		while (current != null && !end_of_statements)
-		{
+		while (current != null && !end_of_statements) {
 			end_of_statements = checkStatement(current, table);
-			if (end_of_statements)
-			{
+			if (end_of_statements) {
 				return current;
 			}
 			current = SemanticsUtils.getNextChild(current);
@@ -45,15 +43,22 @@ public class StatementChecker
 				|| node.getText().contentEquals("drank"))
 		{
 			validateAteAndDrank(node, symbolTable);
+			if (node.getText().contentEquals("ate")) {
+				StatementsCodeGeneratorMagda.writeAteCode(node);
+			} else {
+				StatementsCodeGeneratorMagda.writeDrankCode(node);
+			}
 			return false;
 		} else if (node.getText().contentEquals("became"))
 		{
 			validateBecame(node, symbolTable);
+			StatementsCodeGeneratorMagda.writeBecameCode(node);
 			return false;
 		} else if (node.getText().contentEquals("spoke")
 				|| node.getText().contentEquals("said"))
 		{
 			validatePrint(node, symbolTable);
+			StatementsCodeGeneratorMagda.writePrintStatementCode(node);
 			return false;
 		} else if (node.getText().contentEquals("what"))
 		{
@@ -62,6 +67,7 @@ public class StatementChecker
 		} else if (node.getText().contentEquals("found"))
 		{
 			validateFound(node.getChild(0), symbolTable);
+			StatementsCodeGeneratorMagda.writeFoundCode(node);
 			return false;
 		} else if (node.getText().contentEquals("had"))
 		{
@@ -98,125 +104,105 @@ public class StatementChecker
 		return true;
 	}
 
-	private static void validateFound(Tree node, SymbolTable symbolTable)
-	{
-		FunctionSemanticsChecker.returnType = 
-			SemanticsUtils.getValueType(node, symbolTable);
+	private static void validateFound(Tree node, SymbolTable symbolTable) {
+		FunctionSemanticsChecker.returnType = SemanticsUtils.getValueType(node,
+				symbolTable);
 	}
 
-	private static void validateCodeBlock(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateCodeBlock(Tree node, SymbolTable symbolTable) {
 		CodeBlockSTValue codeVal = new CodeBlockSTValue(symbolTable);
-		symbolTable.insert("l" + node.getLine() + "c"
-				+ node.getCharPositionInLine(), codeVal);
+		symbolTable.insert(
+				"l" + node.getLine() + "c" + node.getCharPositionInLine(),
+				codeVal);
 		symbolTable = codeVal.getTable();
 		node = checkAllStatements(node.getChild(0), symbolTable);
 		symbolTable = symbolTable.finalizeCurrentScopeLevelTable();
 	}
 
-	private static void validateEither(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateEither(Tree node, SymbolTable symbolTable) {
 		node = node.getChild(0);
 		checkForBooleanExpression(node, symbolTable);
 		node = SemanticsUtils.getNextChild(node);
 		node = checkAllStatements(node, symbolTable);
-		if (node.getText().contentEquals("or"))
-		{
+		if (node.getText().contentEquals("or")) {
 			node = SemanticsUtils.getNextChild(node);
 			node = checkAllStatements(node, symbolTable);
 		}
 	}
 
-	private static void validatePerhaps(Tree current, SymbolTable symbolTable)
-	{
+	private static void validatePerhaps(Tree current, SymbolTable symbolTable) {
 		checkForBooleanExpression(current, symbolTable);
 		current = SemanticsUtils.getNextChild(current);
 		current = checkAllStatements(current, symbolTable);
-		if (current != null && current.getText().contentEquals("maybe"))
-		{
+		if (current != null && current.getText().contentEquals("maybe")) {
 			current = SemanticsUtils.getNextChild(current);
 			checkForBooleanExpression(current, symbolTable);
 			current = SemanticsUtils.getNextChild(current);
 			current = checkAllStatements(current, symbolTable);
 		}
-		if (current != null && current.getText().contentEquals("or"))
-		{
+		if (current != null && current.getText().contentEquals("or")) {
 			current = checkAllStatements(current, symbolTable);
 		}
 	}
 
-	private static void validateHad(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateHad(Tree node, SymbolTable symbolTable) {
 		String var = node.getChild(0).getText();
 
-		if (symbolTable.checkItemIsInCurrentScopeLevel(var))
-		{
+		if (symbolTable.checkItemIsInCurrentScopeLevel(var)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine()
 					+ " Multiple declarations of " + node.getText());
-		} else if (SemanticsUtils.getValueType(node.getChild(1), symbolTable) != DATA_TYPES.NUMBER)
-		{
+		} else if (SemanticsUtils.getValueType(node.getChild(1), symbolTable) != DATA_TYPES.NUMBER) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + " : Array length "
 					+ node.getChild(1).getText() + " is not a number.");
-		} else
-		{
-			SymbolTableValue val = new VariableSTValue(DATA_TYPES
-					.valueOf(("array_" + node.getChild(2)).toUpperCase()), true);
+		} else {
+			SymbolTableValue val = new VariableSTValue(
+					DATA_TYPES.valueOf(("array_" + node.getChild(2))
+							.toUpperCase()), true);
 			symbolTable.insert(node.getChild(0).getText(), val);
 		}
 	}
 
-	private static void validateWhat(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateWhat(Tree node, SymbolTable symbolTable) {
 		String var = node.getChild(0).getText();
-		if (!symbolTable.checkItemWasDeclaredBefore(var))
-		{
+		if (!symbolTable.checkItemWasDeclaredBefore(var)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + ": " + var
 					+ " out of scope");
 		}
 
-		else
-		{
-			try
-			{
+		else {
+			try {
 				((VariableSTValue) symbolTable.lookup(node.getChild(0)
 						.getText())).setInitialised(true);
-			} catch (ClassCastException e)
-			{
+			} catch (ClassCastException e) {
 				System.err.println("Line " + node.getLine() + ": "
-					+ node.getCharPositionInLine() + ": " + 
-					" Invalid variable for read statement.");
+						+ node.getCharPositionInLine() + ": "
+						+ " Invalid variable for read statement.");
 			}
 		}
 	}
 
-	private static void validatePrint(Tree node, SymbolTable symbolTable)
-	{
+	private static void validatePrint(Tree node, SymbolTable symbolTable) {
 		SemanticsUtils.getValueType(node.getChild(0), symbolTable);
 	}
 
-	private static void validateBecame(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateBecame(Tree node, SymbolTable symbolTable) {
 		String var = node.getChild(0).getText();
-		if (node.getChild(0).getText().contentEquals("piece"))
-		{
+		if (node.getChild(0).getText().contentEquals("piece")) {
 			ArrayElemCheck.checkArrayElem(node.getChild(0), symbolTable);
-		} else if (!symbolTable.checkItemWasDeclaredBefore(var))
-		{
+		} else if (!symbolTable.checkItemWasDeclaredBefore(var)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + ": " + var
 					+ " out of scope");
 		} else if ((symbolTable.lookup(node.getChild(0).getText())).getType() != SemanticsUtils
 				.getValueType(node.getChild(node.getChildCount() - 1),
-						symbolTable))
-		{
+						symbolTable)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + ": Types of " + var
 					+ " and the subcribed value don't match.");
-		} else
-		{
+		} else {
 
 			((VariableSTValue) symbolTable.lookup(node.getChild(0).getText()))
 					.setInitialised(true);
@@ -224,36 +210,32 @@ public class StatementChecker
 		}
 	}
 
-	private static void validateAteAndDrank(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateAteAndDrank(Tree node, SymbolTable symbolTable) {
 		String var = node.getChild(0).getText();
-		if (!symbolTable.checkItemWasDeclaredBefore(var))
-		{
+		if (!symbolTable.checkItemWasDeclaredBefore(var)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + " : " + var
 					+ " out of scope.");
-		} else if ((symbolTable.lookup(node.getChild(0).getText())).getType() != DATA_TYPES.NUMBER)
-		{
+		} else if ((symbolTable.lookup(node.getChild(0).getText())).getType() != DATA_TYPES.NUMBER) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + " : " + var
 					+ " not a number.");
 		} else if (!((VariableSTValue) symbolTable.lookup(node.getChild(0)
-				.getText())).isInitialised())
-		{
+				.getText())).isInitialised()) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + ": " + var
 					+ " not initialised yet");
 		}
-		String out = "@."+ var + " = private unnamed_addr constant [" + (var.length() + 1) + " x i8] c\"" + "VAL_FROM_TABLE" + '\\' + '0' +"0\", align 1" ;
+		String out = "@." + var + " = private unnamed_addr constant ["
+				+ (var.length() + 1) + " x i8] c\"" + "VAL_FROM_TABLE" + '\\'
+				+ '0' + "0\", align 1";
 		System.out.println(out);
 	}
 
-	private static void validateWasStatement(Tree node, SymbolTable symbolTable)
-	{
+	private static void validateWasStatement(Tree node, SymbolTable symbolTable) {
 		String var = node.getChild(0).getText();
 
-		if (symbolTable.checkItemIsInCurrentScopeLevel(var))
-		{
+		if (symbolTable.checkItemIsInCurrentScopeLevel(var)) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine()
 					+ " Multiple declarations of " + var);
@@ -261,27 +243,25 @@ public class StatementChecker
 
 		else if (node.getChildCount() == 3
 				&& (SemanticsUtils.getValueType(node.getChild(2), symbolTable) != (SemanticsUtils
-						.getReturnType(node.getChild(1)))))
-		{
+						.getReturnType(node.getChild(1))))) {
 			System.err.println("Line " + node.getLine() + ": "
 					+ node.getCharPositionInLine() + " : Data types of " + var
 					+ " and subscribed value don't match.");
 		}
 
-		else
-		{
+		else {
 
 			boolean initialised = node.getChildCount() == 3;
-			symbolTable.insert(var, new VariableSTValue(SemanticsUtils
-					.getReturnType(node.getChild(1)), initialised));
+			symbolTable.insert(
+					var,
+					new VariableSTValue(SemanticsUtils.getReturnType(node
+							.getChild(1)), initialised));
 		}
 	}
 
-	private static void checkForBooleanExpression(Tree expr, SymbolTable table)
-	{
+	private static void checkForBooleanExpression(Tree expr, SymbolTable table) {
 		DATA_TYPES expr_type = ExpressionChecker.getExpressionType(expr, table);
-		if (expr_type != DATA_TYPES.BOOLEAN)
-		{
+		if (expr_type != DATA_TYPES.BOOLEAN) {
 			System.err.println("Line: " + expr.getLine() + " :"
 					+ expr.getCharPositionInLine() + " Invalid Expression "
 					+ "for the statement. Expected BOOLEAN, Actual Type "
