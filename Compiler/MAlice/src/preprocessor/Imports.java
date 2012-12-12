34c36c99c38e9152f9a25d8a5b867846ebab2737
Varun Verma <vv311@ic.ac.uk>
2012-12-12 01:44:14 +0000

    cleaned up the checking of cyclic dependencies

--- Compiler/MAlice/src/preprocessor/Imports.java
+++ Compiler/MAlice/src/preprocessor/Imports.java
@@ -20,21 +20,26 @@ import semantics_checks.SemanticsUtils;
 public class Imports
 {
 	private static Set<String> imports = new HashSet<String>();
+
 	public static Tree checkImports(Tree node) throws IOException,
 			RecognitionException
 	{
 		Tree current = node.getChild(0);
 		if (current.getText().contentEquals("bring"))
 		{
-			if(imports.contains(current.getChild(current.getChildCount()-1).getText()))
+			if (imports.contains(current.getChild(current.getChildCount() - 1)
+					.getText()))
 			{
-				System.err.println("ERROR: Cyclic dependencies encountered " +
-						"for file <" + current.getChild(current.getChildCount()-1).getText()
-						+ ">");
-				SemanticVerifier.failed=true;
+				System.err.println("ERROR: Cyclic dependencies encountered "
+						+ "for file <"
+						+ current.getChild(current.getChildCount() - 1)
+								.getText() + ">");
+				SemanticVerifier.failed = true;
 				return null;
-			} else {
-				imports.add(current.getChild(current.getChildCount()-1).getText());
+			} else
+			{
+				imports.add(current.getChild(current.getChildCount() - 1)
+						.getText());
 			}
 			current = importFunctions(current);
 			current = SemanticsUtils.getNextChild(current);
@@ -46,16 +51,35 @@ public class Imports
 	private static Tree importFunctions(Tree node) throws IOException,
 			RecognitionException
 	{
-		if (node==null) return null;
+		if (node == null)
+			return null;
 		Tree current = node;
 
 		while (current != null && current.getText().contentEquals("bring"))
 		{
+			Tree tree = null;
+			CharStream input = new ANTLRFileStream(
+			// new java.io.File( "." ).getCanonicalPath() + "/"
+					"c:/Users/varun/Documents/Malice/"
+							+ "malice_new_clone/Tests For Extension"
+							+ "/"
+							+ current.getChild(1).getText().substring(1,
+									current.getChild(1).getText().length() - 1)
+							+ ".alice");
+			malice_grammarLexer lexer = new malice_grammarLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			if (lexer.getNumberOfSyntaxErrors() == 0)
+			{
+				malice_grammarParser parser = new malice_grammarParser(tokens);
+				program_return prog = parser.program();
+				if (parser.getNumberOfSyntaxErrors() == 0)
+				{
+					tree = (Tree) prog.getTree();
+				}
+			}
 			if (current.getChild(0).getText().contentEquals("everything"))
 			{
-				importAllFunctions(node, current.getChild(1).getText()
-						.substring(1,
-								current.getChild(1).getText().length() - 1));
+				attachAllFunctionToTree(node.getParent(), checkImports(tree));
 				current = SemanticsUtils.getNextChild(current);
 			}
 		}
@@ -63,46 +87,23 @@ public class Imports
 		return current;
 	}
 
-	private static void importAllFunctions(Tree node, String filepath)
-			throws IOException, RecognitionException
-	{
-		CharStream input = new ANTLRFileStream(
-		// new java.io.File( "." ).getCanonicalPath() + "/"
-				"c:/Users/varun/Documents/Malice/"
-						+ "malice_new_clone/Tests For Extension" + "/"
-						+ filepath + ".alice");
-		malice_grammarLexer lexer = new malice_grammarLexer(input);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		if (lexer.getNumberOfSyntaxErrors() == 0)
-		{
-			malice_grammarParser parser = new malice_grammarParser(tokens);
-			program_return prog = parser.program();
-			if (parser.getNumberOfSyntaxErrors() == 0)
-			{
-				Tree tree = (Tree) prog.getTree();
-				attachAllFunctionToTree(node.getParent(), checkImports(tree));
-			}
-		}
-	}
-
 	private static void attachAllFunctionToTree(Tree node, Tree toAttach)
 	{
-		if (toAttach==null) return;
-		Tree current = (toAttach.getText() == null) ? toAttach.getChild(0)
-				: toAttach;
-//		Tree temp = (node.getText() == null) ? node.getChild(0) : node;
-//		while (temp != null && !temp.getText().contentEquals("bring"))
-//		{
-//			temp = SemanticsUtils.getNextChild(temp);
-//		}
+		if (toAttach == null)
+			return;
+		// Tree temp = (node.getText() == null) ? node.getChild(0) : node;
+		// while (temp != null && !temp.getText().contentEquals("bring"))
+		// {
+		// temp = SemanticsUtils.getNextChild(temp);
+		// }
 		int curr_child = 0;
 		int number_of_children = node.getChildCount();
 		while (curr_child != number_of_children)
 		{
 			if (!node.getChild(curr_child).getText().contentEquals("bring"))
 			{
-//				insertChildInIndex(node, temp.getChildIndex(), current);
-				//insertChildInIndex(node, current);
+				// insertChildInIndex(node, temp.getChildIndex(), current);
+				// insertChildInIndex(node, current);
 				node.addChild(node.getChild(curr_child));
 			}
 			curr_child++;
@@ -110,23 +111,23 @@ public class Imports
 		}
 	}
 
-//	private static void insertChildInIndex(Tree node,Tree toInsert)
-//	{
-//		// Tree curr = node.getChild(childIndex);
-//		// int numberOfChilds = node.getChildCount();
-//		// childIndex++;
-//		// Tree next = node.getChild(childIndex);
-//		// while(childIndex<numberOfChilds)
-//		// {
-//		// node.freshenParentAndChildIndexes();
-//		// node.setChild(childIndex, curr);
-//		// curr = next;
-//		// next = node.getChild(childIndex);
-//		// childIndex++;
-//		// }
-//
-//		node.addChild(toInsert);
-//		node.freshenParentAndChildIndexes();
-//		// node.addChild(curr);
-//	}
+	// private static void insertChildInIndex(Tree node,Tree toInsert)
+	// {
+	// // Tree curr = node.getChild(childIndex);
+	// // int numberOfChilds = node.getChildCount();
+	// // childIndex++;
+	// // Tree next = node.getChild(childIndex);
+	// // while(childIndex<numberOfChilds)
+	// // {
+	// // node.freshenParentAndChildIndexes();
+	// // node.setChild(childIndex, curr);
+	// // curr = next;
+	// // next = node.getChild(childIndex);
+	// // childIndex++;
+	// // }
+	//
+	// node.addChild(toInsert);
+	// node.freshenParentAndChildIndexes();
+	// // node.addChild(curr);
+	// }
 }
