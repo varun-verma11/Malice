30f09a260c479dffaf237716b90f10efd8ca5d56
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-12-14 11:12:32 +0000

    To get recent pull

--- Compiler/MAlice/src/symbol_table/FunctionSTValue.java
+++ Compiler/MAlice/src/symbol_table/FunctionSTValue.java
@@ -2,12 +2,17 @@ package symbol_table;
 
 import java.util.ArrayList;
 
-
 public class FunctionSTValue extends SymbolTableValue
 {
 	SymbolTable symbolTable;
 	ArrayList<DATA_TYPES> args;
 	
+	/**
+	 * FunctionSTValue Constructor for void-returning functions
+	 * 
+	 * @param currSymTable		Current SymbolTable
+	 * @param args				Function arguments
+	 */
 	public FunctionSTValue(SymbolTable currSymTable, ArrayList<DATA_TYPES> args) 
 	{
 		this.type = null;
@@ -17,6 +22,13 @@ public class FunctionSTValue extends SymbolTableValue
 		this.args = args;
 	}
 	
+	/**
+	 * FunctionSTValue Constructor for returning functions
+	 * 
+	 * @param return_type		Data type of return value
+	 * @param currSymTable		Current SymbolTable
+	 * @param args				Function arguments
+	 */
 	public FunctionSTValue(DATA_TYPES return_type, SymbolTable currSymTable, ArrayList<DATA_TYPES> args)
 	{
 		this.type = return_type ;
@@ -26,16 +38,26 @@ public class FunctionSTValue extends SymbolTableValue
 		this.args = args;
 	}
 
+	
+	/**
+	 * Returns the symbol table associated with FunctionSTValue
+	 * 
+	 * @return		FSTValue's SymbolTable
+	 */
 	public SymbolTable getTable()
 	{
 		return symbolTable ;
 	}	
 	
+	
+	/**
+	 * Returns the arguments list associated with FunctionSTValue
+	 * 
+	 * @return		FSTValue's arguments in the form of an array-list
+	 */
 	public ArrayList<DATA_TYPES> getArgs() 
 	{
 		return args;
 	}
 	
 }
-
-
